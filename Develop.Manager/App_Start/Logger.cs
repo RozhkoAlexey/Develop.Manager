3937d969b1746c8308bc39d54c14c839ff333837
Rozhko Alexey <morda_a@inbox.ru>
2018-08-17 10:07:30 +0300

    Added logger in project.

---
+++ Develop.Manager/App_Start/Logger.cs
@@ -0,0 +1,15 @@
+﻿namespace Develop.Manager.App_Start
+{
+    using log4net;
+    using log4net.Config;
+
+    public class Logger
+    {
+        public static ILog Log { get; } = LogManager.GetLogger("LOGGER");
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
--- Develop.Manager/Global.asax.cs
+++ Develop.Manager/Global.asax.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Routing;
-
-namespace Develop.Manager
+﻿namespace Develop.Manager
 {
+    using Develop.Manager.App_Start;
+    using System;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            Logger.Log.Error(exception);
+        }
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Logger.InitLogger();
         }
     }
 }
